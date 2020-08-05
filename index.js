4a799d7ada384f82315e94f06da362754d4f2a8c
qngapparat <jakob.wallnoefer@outlook.com>
2020-08-05 12:48:21 +0200

    finish google, amazon

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 // TODO error msgs
-// TODO add supprot for google auth token 
-
+// TODO error handling
 function checkHTTPStatus(res) {
   if (res.ok) { // res.status >= 200 && res.status < 300
     return res;
@@ -11,42 +10,63 @@ function checkHTTPStatus(res) {
   }
 }
 
-function callAmazon(functionName, payload, options) {
-  if (options.region == null) throw new Error("options.region (string) was unspecified, which is needed to call Amazon functions.")
-  const lambda = new (require('aws-sdk')).Lambda({ region: options.region })
+function callAmazon(functionName, payload, amazonOptions) {
+  if (amazonOptions.region == null) {
+    throw new Error("options.amazon.region (string) was unspecified.")
+  }
+  const lambda = new (require('aws-sdk')).Lambda({ region: amazonOptions.region })
   return lambda
     .invoke({
       FunctionName: functionName,
       Payload: JSON.stringify(payload)
     })
-    .promise().then(p => p.Payload)
+    .promise()
+    .then(p => p.Payload)
+    .then(p => JSON.parse(p))
 }
 
-function callGoogle(functionName, payload, options) {
+function callGoogle(functionName, payload, googleOptions) {
+  // TODO add support for google auth token 
   const fetch = require('node-fetch')
-  if (options.googleProjectName == null) throw new Error("options.googleProjectName (string) was unspecified, which is needed to call Google functions.")
-  if (options.region == null) throw new Error("options.region (string) was unspecified, which is needed to call Google functions.")
-  const url = `https://${options.region}-${options.googleProjectName}.cloudfunctions.net/${functionName}`
+  if (googleOptions.projectName == null) {
+    throw new Error("options.google.projectName (string) was unspecified.")
+  }
+  if (googleOptions.region == null) {
+    throw new Error("options.google.region (string) was unspecified.")
+  }
+  const url = `https://${googleOptions.region}-${googleOptions.projectName}.cloudfunctions.net/${functionName}`
   return fetch(url, { method: 'POST', body: JSON.stringify(payload) })
     .then(checkHTTPStatus)
     .then(res => res.json())
 }
 
 
+/**
+ * Call a FaaS function on Amazon or Google
+ * @returns {Object} What the FaaS function returned
+ * @param {string} functionName The name of the Amazon/Google function. For Amazon, it can also be an `ARN` or a `partial ARN`
+ * @param {Object} payload The payload that will be passed
+ * @param {Object} options Should contain either the `amazon` or `google` field. `amazon` in turn a `region` field. `google` in turn a `projectName` and `region` field.
+ */
 function call(functionName, payload, options) {
-  if (payload == null) throw new Error("Field options.payload was unspecified, but it must be an object.")
-  return callGoogle(functionName, payload, options)
+  if(options.amazon && options.google) {
+    throw new Error("Specify either 'options.amazon' or 'options.google', but not both. Cannot decide which to call.")
+  }
+  if (payload == null) throw new Error("Field 'payload' was unspecified, but it must be an object.")
+  if (functionName == null) throw new Error("Field 'functionName' was unspecified, but it must be a string.")
+
+  if(options.amazon) {
+    return callAmazon(functionName, payload, options.amazon)
+  }
+  else if(options.google) {
+    return callGoogle(functionName, payload, options.google)
+  }
+  else {
+    throw new Erorr("Neither options.amazon nor options.google specified.")
+  }
 }
-
-
-call('IBMTEST4', { msg: "ayy" }, { googleProjectName: 'firstnodefunc', region: 'us-central1' })
-  .then(console.log)
-  .catch(e => console.log(e))
 
 
 module.exports = {
   call
 }
-
-
-curl -X POST "https://us-central1-firstnodefunc.cloudfunctions.net/IBMTEST4" -H "Content-Type:application/json" --data '{"name":"Keyboard Cat"}'
